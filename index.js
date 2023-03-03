10b20904c5059c4b6842cec18ddd18b056e4ac15
Michael A Clark <77692169+mikec7592@users.noreply.github.com>
2023-03-03 12:52:08 -0800

    added edit route and ability to update via put request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Hike = require('./models/hike');
 
 mongoose.connect('mongodb://localhost:27017/yelp-hike', {
@@ -22,6 +23,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
 
 
 app.get('/', (req, res) => {
@@ -46,6 +48,17 @@ app.post('/hikes', async (req, res) => {
 app.get('/hikes/:id', async (req, res) => {
     const hike = await Hike.findById(req.params.id)
     res.render('hikes/show', { hike });
+})
+
+app.get('/hikes/:id/edit', async (req, res) => {
+    const hike = await Hike.findById(req.params.id)
+    res.render('hikes/edit', { hike });
+})
+
+app.put('/hikes/:id', async (req, res) => {
+    const { id } = req.params;
+    const hike = await Hike.findByIdAndUpdate(id, { ...req.body.hike});
+    res.redirect(`/hikes/${hike._id}`)
 })
 
 const PORT = 3000
