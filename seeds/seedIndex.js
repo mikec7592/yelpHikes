b8447be44fe16b39b9c2faa5ab660fcbd10e4662
Michael A Clark <77692169+mikec7592@users.noreply.github.com>
2023-03-17 12:45:40 -0700

    Added some bootstrap styling to form pages. Added inputs for description and price on new forms.

--- seeds/seedIndex.js
+++ seeds/seedIndex.js
@@ -23,9 +23,13 @@ const seedDB = async() => {
     await Hike.deleteMany({});
     for (let i = 0; i < 50; i++){
         const randomNum = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10
         const hike = new Hike({
             location: `${cities[randomNum].city}, ${cities[randomNum].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quibusdam praesentium illo, modi omnis voluptatum repellendus sed quasi magnam. Sint, dignissimos omnis sed dolor numquam sunt maxime pariatur recusandae quia.',
+            price
         })
         await hike.save();
     }
