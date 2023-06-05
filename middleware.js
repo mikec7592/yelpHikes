e21890876bc2ae95a8625a1a4217ff24b67b78a5
Michael A Clark <77692169+mikec7592@users.noreply.github.com>
2023-06-05 14:13:33 -0700

    added logic to dynamically display certain elements of nav bar depending on if a user is signed in or not.  added logic to redirect a user back to the page they were viewing if they were prompted to sign in to continue.

--- middleware.js
+++ middleware.js
@@ -1,7 +1,16 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
+        console.log(req.path, req.originalUrl)
         req.flash('error', 'You must be logged in first.')
         return res.redirect('/login');
+    }
+    next();
+}
+
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
     }
     next();
 }
--- routes/users.js
+++ routes/users.js
@@ -3,21 +3,27 @@ const router = express.Router();
 const catchAsync = require('../utilities/catchAsync');
 const User = require('../models/user');
 const passport = require('passport');
+const { storeReturnTo } = require('../middleware');
+
 
 router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', storeReturnTo, catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        req.flash('success', 'Welcome!')
-        res.redirect('/hikes')
+        const redirectUrl = res.locals.returnTo || '/hikes';
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome!')
+            res.redirect(redirectUrl)
+        })
     } catch (e) {
         req.flash('error', e.message);
-        res.redirect('/register');
+        res.redirect(redirectUrl);
     }  
 }));
 
@@ -25,9 +31,20 @@ router.get('/login', (req, res) => {
     res.render('users/login')
 })
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),(req, res) => {
-    req.flash('success', 'Welcome back!');
-    res.redirect('/hikes')
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),(req, res) => {
+    req.flash('success', `Welcome back ${req.user.username}` );
+    const redirectUrl = res.locals.returnTo || '/hikes';
+    res.redirect(redirectUrl);
 })
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/hikes');
+    });
+}); 
 
 module.exports = router;
