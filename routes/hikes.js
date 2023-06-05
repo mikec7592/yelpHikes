12a3387e4c5ae6a866779ede3cddd15e1160cd5e
Michael A Clark <77692169+mikec7592@users.noreply.github.com>
2023-06-05 16:40:29 -0700

    updated hike model to include an author for relationships between user submitted data and a user.

--- routes/hikes.js
+++ routes/hikes.js
@@ -28,15 +28,15 @@ router.get('/new', isLoggedIn, (req, res) => {
 
 router.post('/', isLoggedIn, validateHike, catchAsync(async (req, res, next) => {
     // if (!req.body.hike) throw new ExpressError('Invalid data entry.', 400)
-   
     const hike = new Hike(req.body.hike);
+    hike.author = req.user._id;
     await hike.save();
     req.flash('success', 'Successfully added new hike!')
     res.redirect(`/hikes/${hike._id}`)
 }))
 
 router.get('/:id', catchAsync(async (req, res) => {
-    const hike = await Hike.findById(req.params.id).populate('reviews');
+    const hike = await Hike.findById(req.params.id).populate('reviews').populate('author');
     if (!hike) {
         req.flash('error', 'Could not find hike.');
         return res.redirect('/hikes');
--- seeds/seedIndex.js
+++ seeds/seedIndex.js
@@ -25,6 +25,7 @@ const seedDB = async() => {
         const randomNum = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10
         const hike = new Hike({
+            author: '647a47ea6d7ccb0a941acb9a',
             location: `${cities[randomNum].city}, ${cities[randomNum].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
