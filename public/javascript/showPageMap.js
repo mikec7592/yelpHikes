25e17ac15b6e8a840fb54129f4416a75b210740a
Michael A Clark <77692169+mikec7592@users.noreply.github.com>
2023-07-17 19:00:08 -0700

    Added popup when map marker is clicked displaying name and loaction.  Changed styling of map.

--- public/javascript/showPageMap.js
+++ public/javascript/showPageMap.js
@@ -1,11 +1,17 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    style: 'mapbox://styles/mapbox/outdoors-v12', // style URL
     center: hike.geometry.coordinates, // starting position [lng, lat]
-    zoom: 11, // starting zoom
+    zoom: 12, // starting zoom
 });
 
 new mapboxgl.Marker()
     .setLngLat(hike.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 20 })
+            .setHTML(
+                `<h4>${hike.title}</h4> <p>${hike.location}</p>`
+            )
+    )
     .addTo(map)
