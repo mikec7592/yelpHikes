bf7ece9d16db49bafc6f5ad9dc23907efc79a57e
Michael A Clark <77692169+mikec7592@users.noreply.github.com>
2023-08-03 16:57:21 -0700

    Added package to held defend against db injection.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const ExpressError = require('./utilities/expressError');
 const methodOverride = require('method-override');
 const passport = require('passport');
 const localStrategy = require('passport-local');
+const mongoSanitize = require('express-mongo-sanitize');
 const User = require('./models/user')
 
 const hikeRoutes = require('./routes/hikes')
@@ -42,6 +43,8 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize());
+
 
 const sessionConfig = {
     secret: 'mango',
@@ -66,6 +69,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
