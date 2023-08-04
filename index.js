a40145bb8dc45f5383e9e8bf5612a49719f4842a
Michael A Clark <77692169+mikec7592@users.noreply.github.com>
2023-08-04 16:24:51 -0700

    Added security packages and configured security policy.

--- index.js
+++ index.js
@@ -13,7 +13,37 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const localStrategy = require('passport-local');
 const mongoSanitize = require('express-mongo-sanitize');
-const User = require('./models/user')
+const helmet = require('helmet');
+const User = require('./models/user');
+
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/",
+    `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/",
+    `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`
+];
+const connectSrcUrls = [
+    "https://*.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://events.mapbox.com",
+    `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`
+];
+const fontSrcUrls = [ `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/` ];
+
 
 const hikeRoutes = require('./routes/hikes')
 const reviewRoutes = require('./routes/reviews')
@@ -47,18 +77,39 @@ app.use(mongoSanitize());
 
 
 const sessionConfig = {
+    name: 'MSession',
     secret: 'mango',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
 app.use(session(sessionConfig));
 app.use(flash());
-
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`, //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 // passport for authentication and authorization
 
 app.use(passport.initialize())
